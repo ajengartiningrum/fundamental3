b56183df0cf962dec987465989948bc638abe8a2
ajengartiningrum <a2892641@bangkit.academy>
2021-06-25 20:47:29 +0700

    add class and layout

--- app/src/main/java/com/dicoding/picodiploma/myappsubmis2/FAVOURITE/MappingHelper.kt
+++ app/src/main/java/com/dicoding/picodiploma/myappsubmis2/FAVOURITE/MappingHelper.kt
@@ -2,23 +2,22 @@ package com.dicoding.picodiploma.myappsubmis2.FAVOURITE
 
 import android.database.Cursor
 import android.provider.BaseColumns._ID
+import com.dicoding.picodiploma.myappsubmis2.DATABASE.DatabaseContract
 import java.util.ArrayList
 
 object MappingHelper {
-    fun mapCursorToArrayList(favCursor: Cursor?): ArrayList<User> {
-        val uList = ArrayList<User>()
+    fun mapCursorToArrayList(favCursor: Cursor?): ArrayList<Favorite> {
+        val uList = ArrayList<Favorite>()
 
         favCursor?.apply {
             while (moveToNext()) {
                 val id = getInt(getColumnIndexOrThrow(_ID))
-                val avatar = getString(getColumnIndexOrThrow(UserFavoriteColumn.COLUMN_AVATAR_URL))
-                val username = getString(getColumnIndexOrThrow(UserFavoriteColumn.COLUMN_USERNAME))
-                val name = getString(getColumnIndexOrThrow(UserFavoriteColumn.COLUMN_NAME))
-                val location = getString(getColumnIndexOrThrow(UserFavoriteColumn.COLUMN_LOCATION))
-                val company = getString(getColumnIndexOrThrow(UserFavoriteColumn.COLUMN_COMPANY))
-                val follower = getString(getColumnIndexOrThrow(UserFavoriteColumn.COLUMN_FOLLOWERS))
-                val following = getString(getColumnIndexOrThrow(UserFavoriteColumn.COLUMN_FOLLOWING))
-                uList.add(User( id, avatar, username, name, location, company, follower, following))
+                val avatar = getString(getColumnIndexOrThrow(DatabaseContract.FavoriteColumns.AVATAR))
+                val username = getString(getColumnIndexOrThrow(DatabaseContract.FavoriteColumns.USERNAME))
+                val name = getString(getColumnIndexOrThrow(DatabaseContract.FavoriteColumns.NAME))
+                val location = getString(getColumnIndexOrThrow(DatabaseContract.FavoriteColumns.LOCATION))
+                val company = getString(getColumnIndexOrThrow(DatabaseContract.FavoriteColumns.COMPANY))
+                uList.add(Favorite( id, avatar, username, name, location, company ))
             }
         }
         return uList
